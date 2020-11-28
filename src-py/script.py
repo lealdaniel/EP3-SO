a11cff6fd3b0ff6fbedb5acb58eaacac60608ec8
Joao Lobo <j.lobo.pev@gmail.com>
2020-11-28 15:23:45 -0300

    fix script for creating empty file system

--- src-py/script.py
+++ src-py/script.py
@@ -1,9 +1,13 @@
+
 for i in range(4000, 24900, 4000):
-    print('1'*4000, end='\\')
-print('1'*1000 , end='\\')
+    if i == 4000:
+        print('0' + '1'*3999, end='\\')
+    else :
+        print('1'*4000, end='\\')
+print('1'*900 , end='\\')
 for i in range(4000, 24900, 4000):
        print('-1|'*4000, end='\\')
-print('-1|'*999, end='')
+print('-1|'*899, end='')
 print("-1\\", end='')
 
 for i in range(24900):
